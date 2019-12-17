a303babeae0d55e82d198d634719eacd32afc91c
infinityshades <tash.aneiq@gmail.com>
2019-12-17 09:21:55 +0100

    Even or Odd functions

---
+++ EvenOrOdd.js
@@ -0,0 +1,19 @@
+showNumbers(10)
+function showNumbers (limit){
+    for ( let i = 0; i <= limit; i++){
+        if ( i % 2 ==0){
+        console.log(i, "Even")
+        }
+        else{
+         console.log(i,"Odd")
+        }
+    }
+}
+
+// Mosh way of Coding (Better way)
+// function showNumbers(limit) {
+//     for (let i = 0; i <= limit; i++) {
+//         const message = (i % 2 == 0) ? 'Even' : 'Odd'
+//         console.log(i, message)
+//     }
+// }
