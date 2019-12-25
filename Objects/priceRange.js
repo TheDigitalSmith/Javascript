f8d58deae69c1391448b1b81ed28037365562c9f
infinityshades <tash.aneiq@gmail.com>
2019-12-25 10:55:31 +0100

    priceRange objects

---
+++ Objects/priceRange.js
@@ -0,0 +1,5 @@
+let princeRanges = [
+    { label: '$' , tooltip:"Inexpensive", minPerPerson:0, maxperPerson:10},
+    { label: '$$' , tooltip:"Moderate", minPerPerson:11, maxperPerson:20},
+    { label: '$$' , tooltip:"Expensive", minPerPerson:21, maxperPerson:30},
+]
