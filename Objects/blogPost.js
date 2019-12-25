925f23e4cec9895598ae5d0438984522054868ef
infinityshades <tash.aneiq@gmail.com>
2019-12-25 09:49:50 +0100

    blog post object

---
+++ Objects/blogPost.js
@@ -0,0 +1,15 @@
+const blogPost = {
+    title: "How to cook chicken",
+    body:"Put the chicken in a microwave",
+    author: "Tash",
+    views: 30,
+    comments:[{
+        author:"Tash",
+        body:"Best way to cook chicken I know, thanks Tash"
+    },{
+        author:"Aneiq",
+        body:"Good tip bro!, thanks Tash"
+    }],
+    isLive: true
+}
+console.log(blogPost);
